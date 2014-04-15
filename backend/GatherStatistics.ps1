fb4f40a0d5eeaca784ea4fca6084f828e8e646b1
Artem Anchugov <artem.anchugov@gmail.com>
2014-04-15 23:59:44 +0300

    Solving problem with '+' in urls instead of space. WIP. Updated data. Determening of most expensive request now works.

--- backend/GatherStatistics.ps1
+++ backend/GatherStatistics.ps1
@@ -5,15 +5,19 @@ $pathToLogParser = "C:\Program Files (x86)\Log Parser 2.2\LogParser.exe"
 $yesterdaysLogFileName = [String]::Format("u_ex{0}.log", (get-date).ToUniversalTime().AddDays(-1).ToString('yyMMdd'))
 $yesterdaysLogFilePath = [IO.Path]::Combine($iisLogsFolderPath,  $yesterdaysLogFileName)
 
-Function RunQuery($queryPath, $url)
+Function RunQuery($queryPath, $urlFromLogFile)
 {
 	$urlPostfix = ''
-	If ($url -ne $NULL) {
-		$urlPostfix = '. ' + $url.Replace('/','⁄')
+	If ($urlFromLogFile -ne $NULL) {
+		$urlPostfix = '. ' + $urlFromLogFile.Replace('/','⁄').Replace('+',' ')
 	}
 	$outputFileName = [IO.Path]::GetFileNameWithoutExtension($queryPath) + $urlPostfix + ".csv"	
 	$outputFilePath = [IO.Path]::Combine($outputFolderPath, $outputFileName)
-	Write-Host $url
+	Write-Host $urlFromLogFile
+	$url = $NULL
+	If ($urlFromLogFile -ne $NULL) {
+		$url = $urlFromLogFile.Replace('+',' ')
+	}
 	Write-Host $outputFilePath
 	$params = [String]::Format('file:"{0}"?outputPath="{1}"+logPath="{2}"+url="{3}" -i:IISW3C -o:CSV', $queryPath, $outputFilePath, $yesterdaysLogFilePath, $url)
 	$executable = [String]::Format('& "{0}" {1}', $pathToLogParser, $params)
@@ -24,9 +28,8 @@ Function RunQuery($queryPath, $url)
 
 RunQuery "IIS. Top 15 popular requests.sql"
 RunQuery "IIS. Requests per hour.sql"
-$top15ExpensiveQueryPath = RunQuery "IIS. Top 15 expensive requests.sql"
+$top15ExpensiveUrlsPath = RunQuery "IIS. Top 15 expensive requests.sql"
 
-
-$a = (Get-Content $top15ExpensiveQueryPath)
+$a = (Get-Content $top15ExpensiveUrlsPath)
 For ($i=1; $i -lt $a.Length; $i++)  { RunQuery "IIS. Time-taken depending on time of the day.sql" $a[$i].Split(',')[0]}
 
